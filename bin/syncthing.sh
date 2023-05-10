ceeed5ad1a3968a985841b5cb0fab25b4b1917ab
Rustam Uzairov <mr.uzairov@gmail.com>
2023-05-10 21:30:02 +0000

    syncthing.sh

---
+++ bin/syncthing.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+sudo apt-get update
+sudo apt-get install syncthing
