66ae5c93d194ceddb740a8b38f1b411b7a1a3553
Rustam Uzairov <mr.uzairov@gmail.com>
2023-05-24 11:06:23 +0000

    newsboat

--- bin/newsboat_bookmarks_surfraw.sh
+++ bin/newsboat_bookmarks_surfraw.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
-#             _   _     _      _
-#  __ _  ___ | |_| |__ | | ___| |_ _   _
-# / _` |/ _ \| __| '_ \| |/ _ \ __| | | |
-#| (_| | (_) | |_| |_) | |  __/ |_| |_| |
-# \__, |\___/ \__|_.__/|_|\___|\__|\__,_|
-# |___/
-#       https://www.youtube.com/user/gotbletu
-#       https://twitter.com/gotbletu
-#       https://plus.google.com/+gotbletu
-#       https://github.com/gotbletu
-#       gotbletu@gmail.com
+# this is a simple example script that demonstrates how bookmarking plugins for newsbeuter are implemented
+# (c) 2007 Andreas Krennmair
+# (c) 2016 Alexander Batischev
 
-# info: newsbeuter script to save bookmarks directly to surfraw
-# demo video: https://www.youtube.com/watch?v=rHVfgGTTtNQ
-# references: https://newsbeuter.wordpress.com/2007/08/27/bookmarking/
-# config:
-#   vim ~/.newsbeuter/config
-#     bookmark-cmd "~/.scripts/newsbeuter_bookmarks_surfraw.sh"
-# hotkey:
 #   Ctrl+B to bookmark an article url
 #   Ctrl+G to cancel
 
 url="$1"         # url
 title="$2"       # tags
 description="$3" # nickname (single word only, no spaces)
-echo -e "${description}\t${url}\t;; newsboat ${title}" >>~/.config/surfraw/bookmarks
+# feed_title="$4"
+feed_title="newsboat"
+
+# echo -e "${url}\t${title}\t${description}\t${feed_title}" >>~/bookmarks.txt
+
+echo -e "${description}\t${url}\t;; ${feed_title} ${title}" >>~/.config/surfraw/bookmarks
