24df656b4d86be072804a0886b02a6449d83a7a0
Rustam Uzairov <mr.uzairov@gmail.com>
2024-03-26 02:59:17 +0300

    fix url lazyg

--- reshell.sh
+++ reshell.sh
@@ -175,8 +175,7 @@ function install_lazygit {
 	echo -e "\u001b[7m Installing  \u001b[0m"
 	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" |
 		grep -Po '"tag_name": "v\K[^"]*')
-	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_\
-          ${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 	tar xf lazygit.tar.gz lazygit
 	sudo install lazygit /usr/local/bin
 	rm -rf lazygit.tar.gz
