62eae285ff5ba8c5c54a7c2afef67d270cf3fc2c
Rustam Uzairov <svrvt.zrv@gmail.com>
2024-05-30 15:50:58 +0300

    deleted the dir executable files that I moved to ~/bin/*

--- bin/setup/connman_install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt install connman cmst connman-gtk connman-ui
--- bin/setup/syncthing_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
-sudo apt-get update
-sudo apt-get install syncthing
